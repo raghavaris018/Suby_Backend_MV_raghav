1267d0754e837d857325609ff803e43905d8c0c5
raghavaris018 <raghavaris018@gmail.com>
2024-09-21 00:47:17 +0530

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -7,6 +7,6 @@ router.post('/register', vendorController.vendorRegister);
 router.post('/login', vendorController.vendorLogin);
 
 router.get('/all-vendors', vendorController.getAllVendors);
-router.get('/single-vendor/:vendorId', vendorController.getVendorById);
+router.get('/single-vendor/:Raghav', vendorController.getVendorById);
 
 module.exports = router;
